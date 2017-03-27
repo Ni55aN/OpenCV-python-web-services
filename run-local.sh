f5ffd41148308162e99e9b42604296f06f8a38e6
Vitaliy Stoliarov <huccah@gmail.com>
2017-03-27 17:33:06 +0300

    remove pull

--- run-local.sh
+++ run-local.sh
@@ -1,3 +1,2 @@
-sudo docker pull ni55an/python-opencv-modules
-sudo docker run -p 5000:5000 --name cv-services -d -v $(pwd):/src ni55an/python-opencv-modules bin/sh -c "cd /src && python app.py"
+sudo docker run -p 5000:5000 --name cv-services -d -v $(pwd):/src $1 bin/sh -c "cd /src && python app.py"
 xdg-open http://localhost:5000/demo
