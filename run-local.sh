82d41762e9ae5a34e828873ab8e24926a8673682
Vitaliy Stoliarov <huccah@gmail.com>
2017-04-18 19:24:41 +0300

    pwd path with spaces

--- run-local.sh
+++ run-local.sh
@@ -1,2 +1,2 @@
-sudo docker run -p 5000:5000 --name cv-services -d -v $(pwd):/src $1 bin/sh -c "cd /src && python app.py"
+sudo docker run -p 5000:5000 --name cv-services -d -v "$(pwd)":/src $1 bin/sh -c "cd /src && python app.py"
 xdg-open http://localhost:5000/demo
